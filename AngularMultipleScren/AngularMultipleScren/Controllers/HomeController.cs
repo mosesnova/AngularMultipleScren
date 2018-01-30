4c05ea9d473facc4339ad667efa6db9eeacd989d
Moses Nova <mosesabhisheknova@yahoo.com>
2018-01-31 10:35:06 +1300

    Initial Commit

---
+++ AngularMultipleScren/AngularMultipleScren/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using AngularMultipleScren.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularMultipleScren.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public string Insert_Employee(Employee Employe)
+        {
+            if (Employe != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    Obj.Employees.Add(Employe);
+                    Obj.SaveChanges();
+                    return "Employee Added Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Inserted! Try Again";
+            }
+        }
+        [HttpPost]
+        public string Update_Employee(Employee Employe)
+        {
+
+            TempData["Employe"] = Employe;
+            return "Navigate";
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public JsonResult PopulateData()
+        {
+            return Json(TempData["Employe"], JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Get_AllEmployee()
+        {
+            using (EmployeeEntities Obj = new EmployeeEntities())
+            {
+                List<Employee> Emp = Obj.Employees.ToList();
+                return Json(Emp, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ AngularMultipleScren/AngularMultipleScren/Scripts/AngularCode.js
@@ -0,0 +1,60 @@
+﻿var app = angular.module("myApp", ['angularUtils.directives.dirPagination']);
+app.controller("myCtrl", function ($scope, $http) {
+    $scope.InsertData = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+            $scope.Employe = {};
+            $scope.Employe.Emp_Name = $scope.EmpName;
+            $scope.Employe.Emp_City = $scope.EmpCity;
+            $scope.Employe.Emp_Age = $scope.EmpAge;
+            $http({
+                method: "post",
+                url: "/Home/Insert_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+
+            })
+        }
+    }
+    $scope.UpdateEmp = function (Emp) {
+        $http({
+            method: "post",
+            url: "/Home/Update_Employee",
+            datatype: "json",
+            data: JSON.stringify(Emp)
+        }).then(function (response) {
+            alert(response);
+            window.location.href = 'home/about';
+        })
+
+
+    }
+    $scope.sort = function (keyname) {
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    }
+    $scope.PopulateData = function () {
+        $http({
+            method: "get",
+            url: "/Home/PopulateData"
+        }).then(function (response) {
+            $scope.employees = response.data;
+            $scope.EmpName = $scope.employees.Emp_Name;
+        }, function () {
+
+        })
+    }
+
+    $scope.GetAllData = function () {
+        $http({
+            method: "get",
+            url: "/Home/Get_AllEmployee"
+        }).then(function (response) {
+            $scope.employees = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+})
